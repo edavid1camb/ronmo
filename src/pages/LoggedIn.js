58f7198834f5bb67e864fdd55fb144317e13d788
David Edri <edavid@cambium.co.il>
2021-11-23 11:40:23 +0200

    adf

--- src/pages/LoggedIn.js
+++ src/pages/LoggedIn.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import json from "./data.json";
+
+const Item = ({ item }) => <div>{item.name}</div>;
 
 export const LoggedIn = () => {
-    return (
-        <div>
-            LoggedIn
-        </div>
-    )
-}
+  return (
+    <div>
+      {json.data.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </div>
+  );
+};
